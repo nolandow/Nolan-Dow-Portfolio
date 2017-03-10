d948a32c9d53954e30bec8b7e14af34f693013cf
Nolan Dow <Nolan Dow>
2017-03-10 14:30:49 -0500

    Working on email form

--- form-to-email.php
+++ form-to-email.php
@@ -7,56 +7,7 @@
 	
 	<body>
 		<?php
-			//Error Function
-			function died($error) {
- 				echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-				echo "These errors appear below.<br /><br />";
-				echo $error."<br /><br />";
-				echo "Please go back and fix these errors.<br /><br />";
-				die();
-			}
 			
-			if(!isset($_POST['name']) ||
-				!isset($_POST['email']) ||
-				!isset($_POST['message'])) {
-				died('We are sorry, but there appears to be a problem with the form you submitted.');
-			}
-			
-			$error_message = "";
- 
-			$email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
- 
-			if(!preg_match($email_exp,$email_from)) {
- 
-			 $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
- 
-			 }
- 
-			  $string_exp = "/^[A-Za-z .'-]+$/";
- 
-			if(!preg_match($string_exp,$first_name)) {
- 
-			 $error_message .= 'The First Name you entered does not appear to be valid.<br />';
- 
-			}
- 
-			if(!preg_match($string_exp,$last_name)) {
- 
-			$error_message .= 'The Last Name you entered does not appear to be valid.<br />';
- 
-			}
-		 
-			if(strlen($comments) < 2) {
- 
-			$error_message .= 'The Comments you entered do not appear to be valid.<br />';
-	 
-			}
- 
-			if(strlen($error_message) > 0) {
- 
-				died($error_message);
-		 
-			}
 			
 			$name = $__POST['name'];
 			$visitor_email = $__POST['email'];
