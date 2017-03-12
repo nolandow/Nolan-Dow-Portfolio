eeb608c03a30a58468cfe667f80d317fef4086c0
Nolan Dow <Nolan Dow>
2017-03-12 11:15:41 -0400

    Styling changes and smooth scrolling

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+//Scrolling Navbar
 $(document).ready(function() {
   
   $(window).scroll(function () {
@@ -12,6 +13,22 @@ $(document).ready(function() {
     if ($(window).scrollTop() < 601) {
       $('.masthead-nav').removeClass('navbar-fixed');
       $('#about').css('margin-top',0);
+    }
+  });
+});
+
+//Smooth-scroll
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 300);
+        return false;
+      }
     }
   });
 });
